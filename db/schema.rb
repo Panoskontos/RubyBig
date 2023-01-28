c37507b9a15c3dd6f5cf8e7e3835500b7a37d5a0
Panoskontos <panos96el@gmail.com>
2023-01-28 23:48:29 +0200

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_28_205626) do
+ActiveRecord::Schema.define(version: 2023_01_28_212049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "description"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -30,4 +38,5 @@ ActiveRecord::Schema.define(version: 2023_01_28_205626) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "products"
 end
