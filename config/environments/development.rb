284fc727e71bb50c9b09208b6a5fc8f2d84be536
Panoskontos <panos96el@gmail.com>
2023-03-19 16:14:34 +0200

    f

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,17 +78,7 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'localhost',
-    user_name:            'panos96el@gmail.com',
-    password:             'mtjuojzcsijzciax',
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 
-  }
+
 
 
 
